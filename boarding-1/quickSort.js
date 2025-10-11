82b6d8c350544f4f85bae12035da5001217eebe9
sarathsuryas <sarathsuryasss48@gmail.com>
2025-10-11 16:33:18 +0530

    upedated quicksort

--- boarding-1/quickSort.js
+++ boarding-1/quickSort.js
@@ -16,3 +16,37 @@ function quckSort(arr) {
     return [...quckSort(leftArr),pivot,...quckSort(rightArr)]
 }
 console.log(quckSort(array))
+
+
+
+const swap = (arr, x, y) => {
+  [arr[x], arr[y]] = [arr[y], arr[x]];
+}
+
+const partition = (arr, left, right) => {
+  const pivot = right--;
+  while (left <= right) {
+    while (arr[left] < arr[pivot]) {
+      left++;
+    }
+    while (arr[right] > arr[pivot]) {
+      right--;
+    }
+    if (left <= right) {
+      swap(arr, left, right);
+      left++;
+      right--;
+    }
+  }
+  swap(arr, left, pivot);
+  return left;
+}
+
+const quickSort = (arr, left = 0, right = arr.length - 1) => {
+  if (left < right) {
+    const pivotIndex = partition(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
